61661c4800e48c4cb1649248740d2bbccb1fad34
tee29909 <59160571@go.buu.ac.th>
2019-09-05 16:39:30 +0700

    arraylist

--- src/friend.java
+++ src/friend.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class friend {
 
 	public static void main(String[] args) {
-		dataFriend dataFriend = new dataFriend("admin","admin","mail","admin0","admin0");
+		dataFriend dataFriend;
+		ArrayList<dataFriend> friends = new ArrayList<dataFriend>();
+		friends.add(new dataFriend("admin","admin","admin","admin","admin"));
+		System.out.println(friends.size());
+		System.out.println(friends);
+		login login = new login();
 
 	}
 
--- src/login.java
+++ src/login.java
@@ -1,4 +1,36 @@
+import java.util.Scanner;
 
 public class login {
+	Scanner kb = new Scanner(System.in);
+	public void messageStart() {
+		System.out.println("FriendApp");
+	}
+	public String Username() {
+		System.out.print("Username: ");
+		String username = kb.next();
+		System.out.println();
+		return username;
+	}
+	public String Password() {
+		System.out.print("Password: ");
+		String password = kb.next();
+		System.out.println();
+		return password;
+	}
+	public boolean Check(String Username,String Password) {
+		
+		return true;
+	}
+	public boolean login() {
+		messageStart();
+		String Username = Username();
+		String Password = Password();
+		if(Check(Username,Password)) {
+			return true;
+		}
+		System.out.println("User or Password incorrect.");
+		System.out.println("Please fill in the correct information.");
+		return login();	
+	}
 
 }
