8e0b1d3e5bb05a765be40b801c6f4cc07aa68a6f
tee29909 <59160571@go.buu.ac.th>
2019-09-05 15:23:12 +0700

    first commit

---
+++ src/dataFriend.java
@@ -0,0 +1,16 @@
+
+public class dataFriend {
+	String name;
+	String lasname;
+	String gender; 
+	String username;
+	String password;
+	public dataFriend(String name, String lasname,String gender,String username,String password) {
+		this.name = name;
+		this.lasname = lasname;
+		this.gender = gender; 
+		this.username = username;
+		this.password = password;
+    }
+
+}
